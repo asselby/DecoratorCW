f18fd7fb543e384ddcc54302abf62f7c4082432b
БаймуханбетоваА <@A-106-01>
2018-03-27 21:40:58 +0600

    initial commit

---
+++ FirstProject/MainWin.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace FirstProject
+{
+    partial class MainWin
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // richTextBox
+            // 
+            this.richTextBox.Location = new System.Drawing.Point(42, 30);
+            this.richTextBox.Name = "richTextBox";
+            this.richTextBox.Size = new System.Drawing.Size(199, 88);
+            this.richTextBox.TabIndex = 1;
+            this.richTextBox.Text = "";
+            // 
+            // MainWin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(859, 538);
+            this.Controls.Add(this.richTextBox);
+            this.Name = "MainWin";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox;
+    }
+}
+
---
+++ FirstProject/MainWin.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FirstProject
+{
+    public partial class MainWin : Form
+    {
+        CheckBox multiLineCheckBox = new CheckBox();
+        CheckBox verticalScroll = new CheckBox();
+        CheckBox horizontalScroll = new CheckBox();
+        public event EventHandler CheckedChanged;
+        public MainWin()
+        {
+
+            InitializeComponent();
+            richTextBox.Multiline = false;
+            multiLineCheckBox.Text = "MultiLine";
+            multiLineCheckBox.Location = new System.Drawing.Point(326, 33);
+            multiLineCheckBox.CheckedChanged += new System.EventHandler(CheckBoxCheckedChanged);
+            verticalScroll.Text = "Vertical scroll";
+            verticalScroll.Location = new System.Drawing.Point(325, 73);
+            verticalScroll.CheckedChanged += new System.EventHandler(CheckBoxCheckedChanged);
+            horizontalScroll.Text = "Horizontal scroll";
+            horizontalScroll.Location = new System.Drawing.Point(327, 107);
+            horizontalScroll.CheckedChanged += new System.EventHandler(CheckBoxCheckedChanged);
+            Controls.Add(multiLineCheckBox);
+            Controls.Add(verticalScroll);
+            Controls.Add(horizontalScroll);          
+        }
+
+        private void CheckBoxCheckedChanged(object sender, EventArgs e)
+        {
+            if (multiLineCheckBox.Checked)
+                 richTextBox.Multiline = true;
+            if (verticalScroll.Checked == true)
+                 richTextBox.ScrollBars = RichTextBoxScrollBars.ForcedVertical;
+            else
+                richTextBox.ScrollBars = RichTextBoxScrollBars.None;
+            if (horizontalScroll.Checked == true)
+                 richTextBox.ScrollBars = RichTextBoxScrollBars.ForcedHorizontal;
+        }
+    }
+}
